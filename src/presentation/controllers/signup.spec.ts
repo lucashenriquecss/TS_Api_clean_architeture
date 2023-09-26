29f08bcdd04eb7e8016e5e2ab980e90b01a41d52
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2023-09-26 16:19:40 -0300

    Fix refactor  errors and controllers

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,31 @@
 import { SignUpController } from "./signup";
+import  {MissingParamError} from '../errors/missing-param-error'
+import { InvalidParamError } from "../errors/invalid-param-error";
+import {EmailValidator} from "../protocols/emailValidator"
+
+
+interface SutTypes{
+    sut:SignUpController
+    emailValidatorSub:EmailValidator
+}
+const makeSut = (): SutTypes =>{
+    class EmailValidatorStub implements EmailValidator { 
+        isValid(email: string): boolean{
+            return true
+        }
+    }
+    const  emailValidatorSub = new EmailValidatorStub();
+    const sut =  new SignUpController(emailValidatorSub)
+    return {
+        sut,
+        emailValidatorSub
+    }
+}
+
 
 describe('SignUp Controller' ,()=> {
     test('Should return 400 if no name is provided', ()=> {
-        const sut = new SignUpController();
+        const {sut} = makeSut();
         const httpRequest = {
             body:{
                 email:'email@example.com',
@@ -12,24 +35,75 @@ describe('SignUp Controller' ,()=> {
         }
         const httpResponse = sut.handle(httpRequest);
         expect(httpResponse.statusCode).toBe(400);
-        expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+        expect(httpResponse.body).toEqual(new MissingParamError('name'));
     });
 });
 
 
 describe('SignUp Controller' ,()=> {
     test('Should return 400 if no email is provided', ()=> {
-        const sut = new SignUpController();
+        const {sut} = makeSut();
         const httpRequest = {
             body:{
                 name: 'name',
-             
                 password :'password',
                 passwordConfirmation:'password'
             }
         }
         const httpResponse = sut.handle(httpRequest);
         expect(httpResponse.statusCode).toBe(400);
-        expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+        expect(httpResponse.body).toEqual(new MissingParamError('email'));
+    });
+})
+
+
+describe('SignUp Controller' ,()=> {
+    test('Should return 400 if no password is provided', ()=> {
+        const {sut} = makeSut();
+        const httpRequest = {
+            body:{
+                name: 'name',
+                email:'email@example.com',
+                passwordConfirmation:'password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new MissingParamError('password'));
+    });
+})
+describe('SignUp Controller' ,()=> {
+    test('Should return 400 if no passwordConfirmation is provided', ()=> {
+        const {sut} = makeSut();
+        const httpRequest = {
+            body:{
+                name: 'name',
+                email:'email@example.com',
+                password :'password',
+               
+            }
+        }
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'));
+    });
+})
+
+
+describe('SignUp Controller' ,()=> {
+    test('Should return 400 if an invalid email is provided', ()=> {
+        const {sut, emailValidatorSub} = makeSut()
+        jest.spyOn(emailValidatorSub,'isValid').mockReturnValueOnce(false)
+        const httpRequest = {
+            body:{
+                name: 'name',
+                email:'invalid@mail.com',
+                password :'password',
+                passwordConfirmation:'password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new InvalidParamError('email'));
     });
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,19 +1,35 @@
-export class SignUpController{
-    
-    handle(httpRequest:any):any{
+import { HttpRequest,HttpResponse } from "../protocols/http"
+import { MissingParamError } from "../errors/missing-param-error"
+import { badRequest } from "../helpers/http-helper"
+import { Controller } from "../protocols/controller"
 
-        if (!httpRequest.body.name) {
-            return {
-                statusCode:400,
-                body:new Error('Missing param: name')                                                                   
+import {EmailValidator} from "../protocols/emailValidator"
+import { InvalidParamError } from "../errors/invalid-param-error"
+
+export class  SignUpController implements Controller {
+
+    private readonly emailValidator:EmailValidator
+
+    constructor (emailValidator:EmailValidator){
+        this.emailValidator = emailValidator
+    }
+    handle(httpRequest: HttpRequest): HttpResponse {
+        const requiredFields = ['name', 'email','password','passwordConfirmation']
+
+        for (const field of requiredFields) {
+
+            if (!httpRequest.body[field]) {
+                
+                return badRequest(new MissingParamError(field)  ) 
             }
         }
-        if (!httpRequest.body.email) {
-            return {
-                statusCode:400,
-                body:new Error('Missing param: email')
-            }
+        const isValid = this.emailValidator.isValid(httpRequest.body.email);
+        if (!isValid) {
+            return badRequest(new InvalidParamError('email')) 
         }
-        
+        return{
+            statusCode:200,
+            body:{}
+        }
     }
 }
